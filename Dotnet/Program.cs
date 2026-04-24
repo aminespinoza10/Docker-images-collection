cf088049cf599f758c964f69844ed392a65c440d
Amin Espinoza <amin.espinoza@outlook.com>
2026-04-23 20:15:14 -0600

    new method added

--- Dotnet/Program.cs
+++ Dotnet/Program.cs
@@ -1,12 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -32,6 +29,9 @@ app.MapGet("/weatherforecast", () =>
     return forecast;
 })
 .WithName("GetWeatherForecast");
+
+app.MapGet("/currentDate", () => DateTime.Now.ToString("yyyy-MM-dd"))
+    .WithName("GetCurrentDate");
 
 app.Run();
 
